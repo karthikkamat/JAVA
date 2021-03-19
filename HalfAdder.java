72e3b3416d90c97e0a14b049809e6d9c01faf14e
karthikkamat <80611686+karthikkamat@users.noreply.github.com>
2021-03-19 16:21:16 +0530

    JAVA program to implement a Half adder using Bitwise operator

---
+++ HalfAdder.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+class HalfAdder{
+	static int xor(int A,int B){
+		int AxorB;
+		AxorB = A ^ B;
+		return AxorB;
+	}
+	static int and(int A,int B){
+		int AandB;
+		AandB = A ^ B;
+		return AandB;
+	}
+	
+	static void halfAdder(int A, int B){
+		System.out.println("The Sum is: "+xor(A,B));
+		System.out.println("The Carry is: "+and(A,B));
+	}
+	
+	
+	public static void main(String args[]){
+		Scanner num = new Scanner(System.in);
+		System.out.println("Enter 'a'");
+		int a = num.nextInt();
+		System.out.println("Enter 'b'");
+		int b = num.nextInt();
+		
+		halfAdder(a,b);
+	}
+}
