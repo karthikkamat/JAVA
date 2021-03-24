4e5ec36d70637686292f53b145ffa560246b64f7
karthikkamat <80611686+karthikkamat@users.noreply.github.com>
2021-03-24 07:04:47 +0530

    The distance between two points(euclidean distance)

---
+++ Euclidean.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+import java.lang.Math;;
+class Euclidean{
+	public static void main(String args[]){
+		Scanner num = new Scanner(System.in);
+		System.out.println("Enter point x1");
+		double x1 = num.nextDouble();
+		System.out.println("Enter point x2");
+		double x2 = num.nextDouble();
+		System.out.println("Enter point y1");
+		double y1 = num.nextDouble();
+		System.out.println("Enter point y2");
+		double y2 = num.nextDouble();
+		double a = x2-x1;
+		double b = y2-y1;
+		
+		double Eucl = Math.pow((Math.pow(a,2)+Math.pow(b,2)),0.5);
+		System.out.println("The Euclidean Distance is: "+Eucl);
+		
+	}
+	
+}
