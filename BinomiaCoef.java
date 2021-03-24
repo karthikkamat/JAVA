edf91fd844f1aa0b24d9a6706b35079b3cb32e08
karthikkamat <80611686+karthikkamat@users.noreply.github.com>
2021-03-24 11:37:38 +0530

    JAVA- Binomial Coefficient of two positive numbers

---
+++ BinomiaCoef.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+class BinomiaCoef{
+	
+	public static int fact(int n){
+		int fact = 1;
+		while(n!=0){
+			fact = fact * n;
+			n--;
+		}
+		return fact;
+	}
+	
+	
+	public static void main(String args[]){
+		Scanner num = new Scanner(System.in);
+		System.out.println("Enter a number n");
+		int n = num.nextInt();
+		System.out.println("Enter the number k");
+		int k = num.nextInt();
+		int temp = n-k;
+		int binom =(fact(n)/(fact(k)*fact(temp)));
+		System.out.println("The Binomial coefficient of n and k is: "+binom);
+		
+		
+	}	
+}
